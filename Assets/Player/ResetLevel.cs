033094b906708fb7c1671e192c9dbfb786514354
Emil Franczak <251511@edu.p.lodz.pl>
2025-11-04 22:09:56 +0100

    Added boolean that turns off reset (for testing purposes)

--- Assets/Player/ResetLevel.cs
+++ Assets/Player/ResetLevel.cs
@@ -3,20 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class ResetLevel : MonoBehaviour
 {
+
+    public bool resetOff = false;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R) && !resetOff)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
-        if (transform.position.y < -6f)
+        if (transform.position.y < -6f && !resetOff)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Snake"))
+        if (other.CompareTag("Snake") && !resetOff)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
