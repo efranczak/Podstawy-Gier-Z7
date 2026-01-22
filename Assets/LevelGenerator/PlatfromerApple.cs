388ebaad1c3cc0fbe5cd9fe6e03f62651fbae8c3
Emil Franczak <251511@edu.p.lodz.pl>
2026-01-22 12:32:02 +0100

    PlatformerApple animation

--- Assets/LevelGenerator/PlatfromerApple.cs
+++ Assets/LevelGenerator/PlatfromerApple.cs
@@ -10,15 +10,44 @@ public class PlatfromerApple : MonoBehaviour
     private float hue;
     private BoxCollider2D boxCollider;
 
+    private float velocity;
+    private float acceleration;
+    private float maxVelocity = 30.0f;
+
+    private bool isMoving = false;
+
     void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        acceleration = 2.0f;
     }
+
+    void Update()
+    {
+        if (!isMoving) return;
+        acceleration += 0.1f * Time.deltaTime;
+        velocity += acceleration;
+        if (velocity > maxVelocity)
+        {
+            velocity = maxVelocity;
+        }
+        transform.position = new Vector3(transform.position.x + velocity * Time.deltaTime, transform.position.y + 1.5f * Time.deltaTime, transform.position.z);
+
+    }
+
+
+
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !isCollected)
+        {
+            isMoving = true;
+        }
+        if (collision.CompareTag("tail"))
         {
             isCollected = true;
             gameObject.SetActive(false);
@@ -38,5 +67,10 @@ public class PlatfromerApple : MonoBehaviour
 
         spriteRenderer.color = Color.HSVToRGB(hue, saturation, value);
 
+    }
+
+    void CollectedEvent()
+    {
+        // Placeholder for collected event logic
     }
 }
