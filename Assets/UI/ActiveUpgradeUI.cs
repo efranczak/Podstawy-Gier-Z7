870434beb66d012cca13ee4b210a1eb6bfa5d58b
minikont <syskamateusz07@gmail.com>
2026-02-10 22:32:11 +0100

    kamera podczas używania lian + nowa implementacja consumable

--- Assets/UI/ActiveUpgradeUI.cs
+++ Assets/UI/ActiveUpgradeUI.cs
@@ -7,29 +7,56 @@ public class ActiveUpgradeUI : MonoBehaviour
     public UpgradeData currentUpgrade = null;
     public PlayerSkills playerSkills;
 
+    private ConsumableHandler _consumableHandler;
     private ActiveUpgradesText upgradesText;
 
     private int upgradeCounter = 0;
 
-    private void Start()
+    private void Awake()
     {
         upgradesText = GetComponentInChildren<ActiveUpgradesText>();
+        _consumableHandler = FindAnyObjectByType<ConsumableHandler>();
+
+        if (upgradesText == null)
+            Debug.LogError($"[ActiveUpgradeUI] Brak komponentu ActiveUpgradesText w dzieciach obiektu {gameObject.name}!");
+
         ClearIcon();
     }
-
-
     private void Update()
     {
         if (currentUpgrade == null || upgradesText == null) return;
-        switch (currentUpgrade.type)
+
+        if (currentUpgrade.category == UpgradeCategory.Permanent)
         {
-            case UpgradeType.DoubleJump: upgradesText.SetText(playerSkills.PlayerJumps.ToString()); break;
-            case UpgradeType.Dash: upgradesText.SetText(playerSkills.PlayerDashes.ToString()); break;
-            case UpgradeType.WallJump: upgradesText.SetText((playerSkills.SameWallJumpMaxAmount+1).ToString()); break;
+            if (playerSkills != null)
+            {
+                switch (currentUpgrade.type)
+                {
+                    case UpgradeType.DoubleJump: upgradesText.SetText(playerSkills.PlayerJumps.ToString()); break;
+                    case UpgradeType.Dash: upgradesText.SetText(playerSkills.PlayerDashes.ToString()); break;
+                    case UpgradeType.WallJump: upgradesText.SetText((playerSkills.SameWallJumpMaxAmount + 1).ToString()); break;
+                    default: upgradesText.HideText(); break;
+                }
+            }
         }
 
-    }
+        else if (currentUpgrade.category == UpgradeCategory.Consumable)
+        {
+            if (_consumableHandler != null)
+            {
+                float timeLeft = _consumableHandler.GetTimeRemaining();
 
+                if (timeLeft > 0)
+                {
+                    upgradesText.SetText(Mathf.CeilToInt(timeLeft).ToString());
+                }
+                else
+                {
+                    upgradesText.HideText();
+                }
+            }
+        }
+    }
     public bool SetIcon(UpgradeData upgrade)
     {
         if (upgrade == null)
@@ -43,6 +70,7 @@ public class ActiveUpgradeUI : MonoBehaviour
         icon.texture = upgrade.iconUI.texture;
         icon.color = Color.white;
         currentUpgrade = upgrade;
+        if (upgradesText != null) upgradesText.SetText("");
         upgradeCounter++;
         return true;
         }
@@ -54,7 +82,6 @@ public class ActiveUpgradeUI : MonoBehaviour
         icon.color = new Color(0, 0, 0, 0);
         currentUpgrade = null;
         upgradeCounter = 0;
+        if (upgradesText != null) upgradesText.HideText();
     }
-
-
 }
