0ea601291752d94ac17d1f50511340bdc3056910
TymonSmykowski <252956@edu.p.lodz.pl>
2026-02-12 15:15:32 +0100

    Shop

--- Assets/ShopManager.cs
+++ Assets/ShopManager.cs
@@ -9,17 +9,52 @@ public class ShopManager : MonoBehaviour
     public GameObject itemPrefab;
 
     [Header("Mo¿liwe ulepszenia")]
-    public UpgradeData[] possibleUpgrades;
+    public UpgradeData jumpData;
+    public UpgradeData dashData;
+    public UpgradeData wallData;
+    public UpgradeData[] optionalUpgrades;
+
+    private GameObject player;
 
     private void Start()
     {
+        player = GameObject.Find("Player");
         ClearOldItems();
         SpawnShopItems();
     }
 
     public void SpawnShopItems()
     {
-        List<UpgradeData> availableData = new List<UpgradeData>(possibleUpgrades);
+        PlayerSkills skills = player.GetComponent<PlayerSkills>();
+        if (skills == null) return;
+
+        List<UpgradeData> availableData = new List<UpgradeData>(optionalUpgrades);
+
+        int ownedPermTypes = 0;
+        if (skills.PlayerJumps > 1) ownedPermTypes++;
+        if (skills.PlayerDashes > 0) ownedPermTypes++;
+        if (skills.SameWallJumpMaxAmount > 0) ownedPermTypes++;
+
+        int maxPermSlots = 2;
+
+        bool hasJump = skills.PlayerJumps > 1;
+        if (hasJump || ownedPermTypes < maxPermSlots)
+        {
+            availableData.Add(jumpData);
+        }
+
+        bool hasDash = skills.PlayerDashes > 0;
+        if (hasDash || ownedPermTypes < maxPermSlots)
+        {
+            availableData.Add(dashData);
+        }
+
+        bool hasWall = skills.SameWallJumpMaxAmount > 0;
+        if (hasWall || ownedPermTypes < maxPermSlots)
+        {
+            availableData.Add(wallData);
+        }
+
 
         foreach (Transform spot in spawnPoints)
         {
@@ -30,15 +65,15 @@ public class ShopManager : MonoBehaviour
 
             GameObject newItem = Instantiate(itemPrefab, spot.position, Quaternion.identity);
 
+            newItem.transform.SetParent(spot);
+
             ShopItemDisplay displayScript = newItem.GetComponent<ShopItemDisplay>();
             if (displayScript != null)
             {
                 displayScript.Setup(selectedData);
             }
 
-
             availableData.RemoveAt(randomIndex);
-
         }
     }
 
